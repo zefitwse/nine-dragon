a1f6782cf52c72a0cdd6ec52ccec3b23d2c99b31
CZengC <zeng1259230551@gmail.com>
2025-05-31 17:56:41 +0800

    Updating: dialogue trigger approach

--- Script/dialogue_trigger.gd
+++ Script/dialogue_trigger.gd
@@ -1,21 +1,30 @@
 class_name DialogueTrigger extends Area2D
 var area_active = false
+@onready var chatting_tips_texture:TextureRect = $"../chatting_tips"
+
 const DIALOGUE_SCENE = preload("res://Scenes/dialogue.tscn")
 var dialogue_instance = null
 var is_dialogue_activa:bool
+
 @export var json_src_path: String
 
 func _ready() -> void:
-	monitorable = true
+	if not chatting_tips_texture:
+		print("chatting_tips_texture not found")
 	
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player and area_active == false:
-		create_dialogue()
 		area_active = true
-
+		chatting_tips_texture.visible = true
+		
 func _on_body_exited(body: Node2D) -> void:
 	if body is Player and area_active == true and is_dialogue_activa == false:
 		area_active = false
+		chatting_tips_texture.visible = false
+		
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("e_pressed") and area_active == true:
+		create_dialogue()
 
 func create_dialogue():
 	dialogue_instance = DIALOGUE_SCENE.instantiate()
@@ -28,7 +37,6 @@ func create_dialogue():
 	if dialogue_instance.has_signal("dialogue_closed"):
 		dialogue_instance.dialogue_closed.connect(_on_dialogue_closed)
 		
-
 	dialogue_instance.pause_game()
 	is_dialogue_activa = true
 	
